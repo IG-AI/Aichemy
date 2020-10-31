19d6c01f55772af495255c61cd102f4042301f08
IG-AI <d.e.agstrand@gmail.com>
2020-10-31 23:40:48 +0100

    Updated setup script

--- ChemInf_0.2/setup.py
+++ ChemInf_0.2/setup.py
@@ -1,4 +1,14 @@
+import os
+from pathlib import Path
+
 from setuptools import setup
+
+models_path = Path('data/amcp_models')
+prediction_path = Path('data/amcp_predictions')
+if not os.path.exists(models_path):
+    os.makedirs(models_path)
+if not os.path.exists(models_path):
+    os.mkdir(prediction_path)
 
 setup(
     setup_requires=['setup.cfg'],
