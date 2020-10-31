dcbb0efc9d8eef4a0c00c83ad643baaa645d7dc1
Daniel Ågstrand <daniel.agstrand.5971@student.uu.se>
2020-10-31 23:18:50 +0100

    Update __main__.py

--- ChemInf_0.2/cheminf/__main__.py
+++ ChemInf_0.2/cheminf/__main__.py
@@ -3,10 +3,9 @@ __version__ = 0.2
 __author__ = "Daniel Ågtrand, Leonard Sparring"
 __email__ = "d.e.agstrand@gmail.com, leo.sparring@gmail.com"
 __date__ = "30/10-2020"
-__credits__ = "This frameworks random forest classifier is based on the cheminf_loop2_20.py aggregated mondrian " \
+__contributors__ = "This frameworks random forest classifier is based on the cheminf_loop2_20.py aggregated mondrian " \
               "conformal predictor written by Ulf Norinder. The neural network classifier is based on " \
-              "01_DNN_CP_tr_te_20.py written by Ulf Norinder and Jin Zhang. The external libraries nonconformist is " \
-              "written by Henrik Linusson that and torchtools is written by Pablo Pernías"
+              "01_DNN_CP_tr_te_20.py written by Ulf Norinder and Jin Zhang."
 """
 22/02-2020, AJL: 
     Added multiclass protocol.
