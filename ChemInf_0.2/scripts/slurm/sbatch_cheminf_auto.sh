23601030f38f7c582e807c60100db7d4148086c5
Daniel Ågstrand <d.e.agstrand@gmail.com>
2020-11-06 22:33:08 +0100

    Updated submit scripts

--- ChemInf_0.2/scripts/slurm/sbatch_cheminf_auto.sh
+++ ChemInf_0.2/scripts/slurm/sbatch_cheminf_auto.sh
@@ -5,24 +5,32 @@
 
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=50G
+#SBATCH --mem=100G
 
 #SBATCH -o /home/x_danag/slurm_output/cheminf_auto.%j.%a.out
 #SBATCH -e /home/x_danag/slurm_output/cheminf_auto.%j.%a.err
 #SBATCH --mail-user daniel.agstrand.5971@student.uu.se
 #SBATCH --mail-type=stop,fail
 
-awk '{print $1=names; print $2=configs}' $SETUP
+#SBATCH --array=1-14
 
-name=$(awk "NR==${SLURM_ARRAY_TASK_ID}" $names)
-config_override=$(awk "NR==${SLURM_ARRAY_TASK_ID}" $configs)
+SETUP=scripts/slurm/projects/inv_nn_different_sampling_setup.csv
+INFILE=cheminf/data/d2_full.csv
+
 work_dir=/proj/carlssonlab/users/x_danag/ChemInf/ChemInf_0.2
 
 cd $work_dir
 
+name=$(awk NR=="$SLURM_ARRAY_TASK_ID" "$SETUP" | cut -d$'\t' -f1)
+config_override=$(awk NR=="$SLURM_ARRAY_TASK_ID" "$SETUP" | cut -d$'\t' -f2)
+
+module use /proj/carlssonlab/envmod; module load FRONTLINE/latest
+
 echo -e "--------------------------------------------------------------------------\n"
-echo    "STARTING JOB NUMBER "${SLURM_ARRAY_TASK_ID}" WITH NAME "$name
+echo    "STARTING JOB NUMBER "$SLURM_ARRAY_TASK_ID" WITH NAME "$name
 echo -e "\n--------------------------------------------------------------------------\n"
 
-python singularity exec /proj/carlssonlab/singularity/frontline.simg \
-python /cheminf auto -cl nn -i $INPUT -n $name -cf $config_override
+singularity exec /proj/carlssonlab/singularity/frontline.simg \
+python cheminf/ auto -cl nn -i $INFILE -n $name -cf $config_override
+
+# sbatch scripts/slurm/sbatch_cheminf_auto.sh
--- ChemInf_0.2/scripts/slurm/sbatch_cheminf_preproc.sh
+++ ChemInf_0.2/scripts/slurm/sbatch_cheminf_preproc.sh
@@ -3,6 +3,7 @@
 #SBATCH --job-name=cheminf_balancing
 #SBATCH --time=12:00:00
 
+#SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=100G
@@ -12,17 +13,22 @@
 #SBATCH --mail-user daniel.agstrand.5971@student.uu.se
 #SBATCH --mail-type=stop,fail
 
+INFILE=d2_full.csv
+
 work_dir=/proj/carlssonlab/users/x_danag/ChemInf/ChemInf_0.2
 
 cd $work_dir
 
+module use /proj/carlssonlab/envmod
+module load FRONTLINE/latest
+
 echo -e "--------------------------------------------------------------------------\n"
-echo    "STARTING BALANCING ""$INFILE"
+echo    "STARTING BALANCING "$INFILE
 echo -e "\n--------------------------------------------------------------------------\n"
 
 srun -N1 --ntasks=1 --cpus-per-task=2 --mem=100G --time=12:00:00 --exclusive \
-python singularity exec /proj/carlssonlab/singularity/frontline.simg \
-python /cheminf preproc balancing -i "$INFILE" -o "cheminf/data/""${INFILE%.*}""_balanced" \
+singularity exec /proj/carlssonlab/singularity/frontline.simg \
+python cheminf/ preproc balancing -i $INFILE -o "cheminf/data/"${INFILE%.*}"_balanced" \
 -ch 100000 -nc $SLURM_CPUS_ON_NODE
 
-# sbatch --export=ALL,INPUT=cheminf/data/d2_full.csv scripts/slurm/sbatch_cheminf_preproc.sh
+# sbatch scripts/slurm/sbatch_cheminf_preproc.sh
