a462f65c51572fa989269648be02f5c9f17e17f1
Daniel Ågstrand <d.e.agstrand@gmail.com>
2020-11-07 22:34:22 +0100

    Updated label_data script

--- ChemInf_0.2/scripts/label_data.sh
+++ ChemInf_0.2/scripts/label_data.sh
@@ -15,13 +15,13 @@ echo "START BALANCING OF: "$infile
 echo -e "\n-------------------------------------------\n"
 
 echo -e "Start counting data in file\n"
-nlines=$(awk -F " " '{print $1}' $infile | wc -l);
+nlines=$(awk -F "\t" '{print $1}' $infile | wc -l);
 threshold=$(echo "(0.01*$nlines) / 1" | bc)
 
 echo -e "Extracting class 1\n"
 head -n $threshold  $infile | awk -F"_" '$1=$1' OFS=" " | sort -k2 -n -t" " | sed "s/ /_/" | \
 sed "s/\t/$(echo -e '\t')1$(echo -e '\t')/" | \
-awk 'BEGIN{srand();} {printf "%06d %s\n", rand()*$threshold, $0;}' | sort -n | cut -c8- \
+ awk 'BEGIN{srand();} {printf "%06d %s\n", rand()*$threshold, $0;}' | sort -n | cut -c8- \
 > $filename"_class1.csv"
 
 echo -e "Extracting class 0\n"
@@ -30,7 +30,7 @@ sed "s/\t/$(echo -e '\t')0$(echo -e '\t')/" | \
 awk 'BEGIN{srand();} {printf "%06d %s\n", rand()*$(($nlines - $threshold)), $0;}' | sort -n | cut -c8- \
 > $filename"_class0.csv"
 
-cat $filename".class1.csv" < $filename".class0.csv" | \
+cat $filename"_class1.csv" < $filename"_class0.csv" | \
 awk 'BEGIN{srand();} {printf "%06d %s\n", rand()*$((2 * $threshold)), $0;}' | sort -n | cut -c8- \
 > $filename"_labeled.csv"
 
