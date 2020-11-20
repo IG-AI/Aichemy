5ddfa6cb6dd5f59595b7bbc24b4c035944f0f536
Daniel Ågstrand <d.e.agstrand@gmail.com>
2020-11-20 14:30:32 +0100

    Small bugfix

--- ChemInf_0.2/scripts/slurm/sbatch_labeling.sh
+++ ChemInf_0.2/scripts/slurm/sbatch_labeling.sh
@@ -16,15 +16,24 @@ work_dir=/proj/carlssonlab/users/x_danag/ChemInf/ChemInf_0.2
 cd $work_dir
 
 full_file=$1
+suffix=$3
+percentage=$2
 
 filename=$(echo "${full_file%.*}")
 
+echo -e "--------------------------------------------------------------------------\n"
+echo    "STARTING LABELING OF "$name
+echo -e "\n--------------------------------------------------------------------------\n"
+
 echo -e "Start counting data in file\n"
 nlines=$(awk -F"\t" '{print $1}' $full_file | wc -l);
-threshold=$(echo "(0.01*$nlines) / 1" | bc)
 
-echo -e "Finding class 1 in file\n"
+echo -e "Finding threshold position\n"
+threshold=$(echo "($percentage*$nlines) / 1" | bc)
+
+echo -e "Extracting class 1 data in file\n"
 cut -f1 $full_file | awk -F"_" '$1=$1' OFS="\t" | sort -k2 -n -t$'\t' | \
-sed "s/\t/_/" | head -n$threshold > $filename"_class1.csv"
+sed "s/\t/_/" | head -n$threshold > $filename"_class1"$suffix".csv"
 
-python scripts/labeling.py $filename"_class1.csv" $full_file $filename"_labeled.csv"
+echo -e "Start labeling data\n"
+python scripts/labeling.py $filename"_class1.csv" $full_file $filename"_"$suffix".csv"
