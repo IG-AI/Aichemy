ffcaea731b775ae7f33cd8251435070e89938b42
Daniel Ågstrand <d.e.agstrand@gmail.com>
2020-12-02 21:48:09 +0100

    Updated submit scripts

--- ChemInf_0.2/scripts/slurm/sbatch_cheminf_auto.sh
+++ ChemInf_0.2/scripts/slurm/sbatch_cheminf_auto.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-#SBATCH --job-name=adam_inv_cheminf
-#SBATCH --time=24:00:00
+#SBATCH --job-name=arch_inv_cheminf
+#SBATCH --time=48:00:00
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=50G
+#SBATCH --mem=300G
 
 #SBATCH -o /home/x_danag/slurm_output/%x.%A.%a.out
 #SBATCH -e /home/x_danag/slurm_output/%x.%A.%a.err
 #SBATCH --mail-user daniel.agstrand.5971@student.uu.se
 #SBATCH --mail-type=none
 
-#SBATCH --array=1-55
+#SBATCH --array=1-35
 
 work_dir=/proj/carlssonlab/users/x_danag/ChemInf/ChemInf_0.2
 
@@ -28,8 +28,8 @@ echo -e "-----------------------------------------------------------------------
 echo    "STARTING JOB NUMBER "$SLURM_ARRAY_TASK_ID" WITH NAME "$name
 echo -e "\n--------------------------------------------------------------------------\n"
 
-srun -N1 --ntasks=1 --cpus-per-task=4 --mem=50G --time=24:00:00 --exclusive \
+srun -N1 --ntasks=1 --cpus-per-task=4 --mem=300G --time=48:00:00 --exclusive \
 singularity exec /proj/carlssonlab/singularity/frontline.simg \
 python cheminf/ auto -cl nn -i $INFILE -n $name -cf $config_override -ch 100000 -nc 4
 
-# sbatch --export=ALL,INFILE=cheminf/data/d2_full.csv,SETUP=scripts/slurm/projects/inv_nn_optimize_Adam_setup.csv scripts/slurm/sbatch_cheminf_auto.sh
+# sbatch --export=ALL,INFILE=cheminf/data/d2_full.csv,SETUP=scripts/slurm/projects/inv_nn_balancing_ratio.csv scripts/slurm/sbatch_cheminf_auto.sh
--- ChemInf_0.2/scripts/slurm/sbatch_cheminf_auto_randomized.sh
+++ ChemInf_0.2/scripts/slurm/sbatch_cheminf_auto_randomized.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-#SBATCH --job-name=conf_noiz_cheminf
+#SBATCH --job-name=RandPred_cheminf
 #SBATCH --time=36:00:00
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=100G
+#SBATCH --mem=150G
 
 #SBATCH -o /home/x_danag/slurm_output/%x.%A.%a.out
 #SBATCH -e /home/x_danag/slurm_output/%x.%A.%a.err
 #SBATCH --mail-user daniel.agstrand.5971@student.uu.se
 #SBATCH --mail-type=none
 
-#SBATCH --array=1-35
+#SBATCH --array=1-4
 
 work_dir=/proj/carlssonlab/users/x_danag/ChemInf/ChemInf_0.2
 
@@ -29,8 +29,8 @@ echo -e "-----------------------------------------------------------------------
 echo    "STARTING JOB NUMBER "$SLURM_ARRAY_TASK_ID" WITH NAME "$name
 echo -e "\n--------------------------------------------------------------------------\n"
 
-srun -N1 --ntasks=1 --cpus-per-task=4 --mem=100G --time=36:00:00 --exclusive \
+srun -N1 --ntasks=1 --cpus-per-task=4 --mem=150G --time=36:00:00 --exclusive \
 singularity exec /proj/carlssonlab/singularity/frontline.simg \
 python cheminf/ auto -i $input -cl nn -n $name -cf $config_override -ch 100000 -nc 4
 
-# sbatch --export=ALL,SETUP=scripts/slurm/projects/inv_nn_noiselevels_setup.csv scripts/slurm/sbatch_cheminf_auto_different_inputs.sh
+# sbatch --export=ALL,SETUP=scripts/slurm/projects/complement.csv scripts/slurm/sbatch_cheminf_auto_randomized.sh
