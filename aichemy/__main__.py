26cc1d09df6cc269ee97090e1f5838b3b693e9fe
Daniel Ågstrand <d.e.agstrand@gmail.com>
2022-05-02 02:33:27 +0200

    Small fixes to the descriptor text
    Modifications of the descriptor text

--- aichemy/__main__.py
+++ aichemy/__main__.py
@@ -1,22 +1,22 @@
 __version__ = "0.1.1"
-__author__ = "Daniel Ågtrand"
+__author__ = "Daniel 'G_A' Ågtrand"
 __email__ = "d.e.agstrand@gmail.com"
-__url__ = "https://github.com/leosparring/AIchemy/tree/master/AIchemy_0.2"
-__platform__ = 'Linux'
-__date__ = "30/10-2020"
+__url__ = "https://github.com/IG-AI/Aichemy/"
+__platform__ = "Unix"
+__date__ = "30/10/2020"
 __contributors__ = "This frameworks random forest classifier is based on the aichemy_loop2_20.py aggregated mondrian " \
                    "conformal predictor written by Ulf Norinder. The neural network classifier is based on " \
                    "01_DNN_CP_tr_te_20.py written by Ulf Norinder and Jin Zhang."
 """
-22/02-2020, AJL: 
+22/02/2020, AJL: 
     Added multiclass protocol.
-24/03-2020, AJL: 
+24/03/2020, AJL: 
     Added (de)compressed data handling.
-08/04-2020, LS:  
+08/04/2020, LS:  
     Revision of decompression and modularity implementation.
-06/07-2020, LS: 
+06/07/2020, LS: 
     Implementation of k-fold cross validation.
-28/09-2020 - 30/10-2020, DA: 
+28/09/2020 - 30/10/2020, DA: 
     Completely revamped the code with a object originated approached, extended the available classifiers with a 
     dynamic neural network, added data utils, completely changed to how the user interact with the program to make it
     more user-friendly and added a setup script, a conda env file and pip env requirements file for easier installation 
